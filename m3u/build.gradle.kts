23ff274f16c7d4f9fbf3a941f4985c97452773c3
mooncrown04 <mooncrown04@gmail.com>
2025-07-08 18:16:57 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     kotlin("android")
     id("com.android.library")
+     id("cloudstream")
+}
+
+cloudstream {
+    language.set("tr")
+    description.set("M3U Yayınları için canlı yayın sağlayıcısı")
+    authors.set(listOf("mooncrown04"))
 }
 
 android {
@@ -31,5 +38,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jsoup:jsoup:1.18.3")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.lagradost:cloudstream3:3.0.0") 
+
     // Diğer bağımlılıklar
 }
