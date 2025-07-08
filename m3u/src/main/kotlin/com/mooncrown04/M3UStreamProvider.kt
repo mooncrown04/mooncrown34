6e87d0272b4adb350413af531a1e007b5bdbe201
mooncrown04 <mooncrown04@gmail.com>
2025-07-08 16:58:26 +0300

    Update M3UStreamProvider.kt

--- m3u/src/main/kotlin/com/mooncrown04/M3UStreamProvider.kt
+++ m3u/src/main/kotlin/com/mooncrown04/M3UStreamProvider.kt
@@ -8,7 +8,7 @@ import kotlin.collections.ArrayList
 
 class M3UStreamProvider : MainAPI() {
     override var name = "M3UStream"
-    override var mainUrl = "https://example.com/myfile.m3u" // 👉 burayı kendi URL’inle değiştir
+    override var mainUrl = "https://raw.githubusercontent.com/mooncrown04/mooncrown34/master/m3u/resources/birlesik.m3u" // 👉 burayı kendi URL’inle değiştir
     override var supportedTypes = setOf(TvType.Live)
 
     // 1 hafta içinde eklenenlere [YENİ] etiketi ver
