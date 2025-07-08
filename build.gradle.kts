ee9b0fcbe44d2ab9fad825592bdc490836e6e72f
mooncrown04 <mooncrown04@gmail.com>
2025-07-08 15:52:49 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.9.10" apply false
+    id("com.android.library") version "8.1.1" apply false
+}
+
 buildscript {
     repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT") // CloudStream plugin
     }
 }
 
@@ -25,53 +29,5 @@ subprojects {
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    extensions.configure<com.lagradost.cloudstream3.gradle.CloudstreamExtension> {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/mooncrown04/mooncrown34")
-        authors = listOf("mooncrown04")
-    }
-
-    extensions.configure<com.android.build.gradle.BaseExtension> {
-        namespace = "com.mooncrown04"
-
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 35
-            compileSdkVersion(35)
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
-
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-            freeCompilerArgs += listOf(
-                "-Xno-call-assertions",
-                "-Xno-param-assertions",
-                "-Xno-receiver-assertions"
-            )
-        }
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    // Diğer yapılandırmalar...
 }
