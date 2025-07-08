aadbe6294622609ac2767a7b49dc83868f8c01e7
mooncrown04 <mooncrown04@gmail.com>
2025-07-08 16:01:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
-plugins {
-    kotlin("jvm") version "1.9.10" apply false
-    id("com.android.library") version "8.1.1" apply false
-}
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import com.android.build.gradle.BaseExtension
 
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven("https://jitpack.io") // CloudStream Gradle Plugin burada
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT") // CloudStream plugin
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
@@ -24,10 +23,64 @@ allprojects {
     }
 }
 
+// Yardımcı fonksiyonlar
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    // Diğer yapılandırmalar...
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "mooncrown04/mooncrown34")
+        authors = listOf("mooncrown04")
+    }
+
+    android {
+        namespace = "com.mooncrown04"
+        compileSdkVersion(34)
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 34
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "1.8"
+                freeCompilerArgs += listOf(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
+            }
+        }
+    }
+
+    dependencies {
+        val implementation by configurations
+        val cloudstream by configurations
+
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.3")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
