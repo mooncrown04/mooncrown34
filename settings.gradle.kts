861d5dc36e0c97c1e73ebf14c65ef1d654c9425e
mooncrown04 <mooncrown04@gmail.com>
2025-07-09 20:40:07 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,22 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
+rootProject.name = "mooncrown34"
+
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+
+val disabled = listOf<String>("__Temel")
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
     }
 }
 
-rootProject.name = "mooncrown34"
-include(":m3u")
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("m3u")
+
+
+
