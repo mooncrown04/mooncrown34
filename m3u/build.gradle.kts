eb5907a32707aaa2fa5e56ba9cc1494a937637b8
mooncrown04 <mooncrown04@gmail.com>
2025-07-09 20:45:51 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,42 +1,18 @@
-plugins {
-    kotlin("android")
-    id("com.android.library")
-    id("cloudstream")
-}
+version = 6
 
 cloudstream {
-    language.set("tr")
-    description.set("M3U Yayınları için canlı yayın sağlayıcısı")
-    authors.set(listOf("mooncrown04"))
-}
+    authors     = listOf("mooncrown04")
+    language    = "tr"
+    description = "( Diğer kanallar mevcuttur."
 
-android {
-    namespace = "com.mooncrown04"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs += listOf(
-            "-Xno-call-assertions",
-            "-Xno-param-assertions",
-            "-Xno-receiver-assertions"
-        )
-    }
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jsoup:jsoup:1.18.3")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.github.recloudstream.cloudstream:cloudstream3:3.3.1")
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=colorhunt.co&sz=%size%"
 }
