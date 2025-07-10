2f38dc9190c17d50d24ac2bdf7e1f0648d429084
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:21:04 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("cloudstream")
+    id("com.android.library") version "8.2.2"
+    kotlin("android") version "1.9.23"
+    id("cloudstream") version "1.0.0"
+}
+
+cloudstream {
+    language.set("tr")
+    description.set("M3U Yayınları için canlı yayın sağlayıcısı")
+    authors.set(listOf("mooncrown04"))
 }
 
 android {
@@ -14,7 +20,18 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://repo.recloudstream.com/releases")
 }
