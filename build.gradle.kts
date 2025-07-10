6b38aa8d96b6aecf5fde0290e0b29f7c4371ddc5
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:02:24 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
+// root build.gradle.kts
 plugins {
-    id("com.android.library") version "8.0.2" apply false
-    kotlin("android") version "1.8.20" apply false
-    id("cloudstream") apply false
+    kotlin("android") version "1.9.10" apply false
+    id("com.android.library") apply false
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://repo.recloudstream.com/releases") }
+        maven("https://jitpack.io")
+        maven("https://repo.recloudstream.com/releases")
     }
 }
-
