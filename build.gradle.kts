14c9ad0708112bce444c24ad3d0ab652c5fab8b3
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:13:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
-// root build.gradle.kts
-plugins {
-    kotlin("android") version "1.9.10" apply false
-    id("com.android.library") apply false
+// build.gradle.kts (ana dizin)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.recloudstream.com/releases")
+    }
 }
 
 allprojects {
