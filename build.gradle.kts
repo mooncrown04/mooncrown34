1911bb09d1e52df0451603582e13c5dc36534636
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 08:54:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.library") version "8.1.0" apply false
-    kotlin("android") version "1.9.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.10" apply false
+    id("cloudstream") version "1.0.0" apply false
 }
 
