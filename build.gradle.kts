8cfe731753adf42c86ec8856cb9a3bec489d4754
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:35:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,1 @@
-// build.gradle.kts (ana dizin)
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://repo.recloudstream.com/releases")
-    }
-}
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://repo.recloudstream.com/releases")
-    }
-}
