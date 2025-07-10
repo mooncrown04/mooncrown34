0ee74b0c4cda3c5affe3e1c8c94eca59a0aa32fe
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:33:26 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,21 @@
-// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.recloudstream.com/releases")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.recloudstream.com/releases")
+    }
+}
+
 rootProject.name = "mooncrown34"
 include(":m3u")
