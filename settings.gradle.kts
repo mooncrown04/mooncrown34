27cdf4121498839df083900c11eb3a4525e74753
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:48:25 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,14 @@ pluginManagement {
         maven("https://jitpack.io")
         maven("https://repo.recloudstream.com/releases")
     }
+
+    plugins {
+        id("org.jetbrains.kotlin.android") version "1.9.23"
+        id("com.android.library") version "8.2.2"
+        id("cloudstream") version "1.0.0" // Cloudstream plugin versiyonunu belirle (gerekirse sabit veya kaldır)
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
