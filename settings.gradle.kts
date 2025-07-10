90b1f269ab1b68c37b205fb24f9e7c307e47d043
mooncrown04 <mooncrown04@gmail.com>
2025-07-10 10:03:25 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://repo.recloudstream.com/releases") }
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "cloudstream") {
-                useModule("com.lagradost:cloudstream:1.0.0")
-            }
-        }
-    }
-}
-
+// settings.gradle.kts
 rootProject.name = "mooncrown34"
 include(":m3u")
