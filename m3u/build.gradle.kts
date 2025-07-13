e8777fd029bff30296105741add05159282348e7
mooncrown04 <mooncrown04@gmail.com>
2025-07-14 01:07:43 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,18 +1,9 @@
-version = 6
+plugins {
+    id("cloudstream-plugin") version "1.0.0" // ya da mevcutsa güncel sürüm
+}
 
 cloudstream {
-    authors     = listOf("mooncrown04")
-    language    = "tr"
-    description = "(BeIN Sport, Tivibu Spor ve Smart Spor için VPN Gerekebilir.) NeonSpor eklentisini de BeIN Sport, Tabii Spor, Tivibu Spor, Smart Spor, S Sport ve Diğer kanallar mevcuttur."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Live")
-    iconUrl = "https://www.google.com/s2/favicons?domain=colorhunt.co&sz=%size%"
+    language.set("tr")
+    description.set("M3U yayın sağlayıcısı")
+    authors.set(listOf("mooncrown04"))
 }
