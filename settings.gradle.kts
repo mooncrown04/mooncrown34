3665d6cb094954e2e6dcd2f83c556e10acc85867
mooncrown04 <mooncrown04@gmail.com>
2025-07-14 00:06:28 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,19 @@
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://repo.recloudstream.com/releases")
-    }
-
-    plugins {
-        id("org.jetbrains.kotlin.android") version "1.9.23"
-        id("com.android.library") version "8.2.2"
-        id("cloudstream") version "1.0.0" // Cloudstream plugin versiyonunu belirle (gerekirse sabit veya kaldır)
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://repo.recloudstream.com/releases")
-    }
-}
-
 rootProject.name = "mooncrown34"
-include(":m3u")
+
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+
+val disabled = listOf<String>("__Temel")
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}
+
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
