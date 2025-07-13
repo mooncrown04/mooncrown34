94a7e937909389c9dc07f12262453f52b32572a0
mooncrown04 <mooncrown04@gmail.com>
2025-07-14 01:30:59 +0300

    Update build.gradle.kts

--- m3u/build.gradle.kts
+++ m3u/build.gradle.kts
@@ -1,9 +1,19 @@
-plugins {
-    id("cloudstream-plugin") version "1.0.0" // ya da mevcutsa güncel sürüm
-}
+version = 31
 
 cloudstream {
-    language.set("tr")
-    description.set("M3U yayın sağlayıcısı")
-    authors.set(listOf("mooncrown04"))
+    authors     = listOf("mooncrown04")
+    language    = "tr"
+    description = "Dizilla tüm yabancı dizileri ücretsiz olarak Türkçe Dublaj ve altyazılı seçenekleri ile 1080P kalite izleyebileceğiniz yeni nesil yabancı dizi izleme siteniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizilla.club&sz=%size%"
 }
+   
